be55f783ea5a262e894c6a2b6ccecdcf357208aa
Marcelo Pessanha <mpessanha1975@gmail.com>
2014-03-04 01:54:16 -0300

    versão 1.0

--- bootstrap/hooks.py
+++ bootstrap/hooks.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 from os.path import join, dirname, pardir, abspath
+from os import mkdir
 from shutil import copy
 import subprocess
 
@@ -10,6 +11,7 @@ ROOT = abspath(join(BOOTSTRAP, pardir))
 # Path where venv will be created. It's imported by bootstrapX.Y.py
 VIRTUALENV = join(BOOTSTRAP, pardir)
 VIRTUALENV_BIN = join(VIRTUALENV, 'bin')
+VIRTUALENV_PROJECT = join(VIRTUALENV, 'project')
 
 WITH_VENV = join(BOOTSTRAP, 'with_venv.sh')
 
@@ -22,9 +24,20 @@ def with_venv(*args):
     cmd.insert(0, WITH_VENV)
     return subprocess.call(cmd)
 
+def with_project(*args):
+	"""
+    Runs the given command inside virtualenv.
+    """
+    diretorios = list(args)
+    for diretorio in diretorios:
+    	try:
+    		return mkdir(diretorio)
+    # Se diretorio existe executa bypass	
+    	except OSError:
+    		pass     	
 
 def after_install(options, home_dir):
     copy(join(BOOTSTRAP, 'postactivate'), VIRTUALENV_BIN)
+    with_project(join(VIRTUALENV_PROJECT, 'project_dir.txt'))
     with_venv('pip', 'install', '-r', join(ROOT, 'requirements.txt'))
     print "Done! Activate your virtualenv: source bin/activate"
-
